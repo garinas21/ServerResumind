786162bae82d96862e0424a238b885b964b93db1
garinas21 <garinakbar78@gmail.com>
2025-09-21 22:05:35 +0700

    Penyesuaian

--- server/src/modules/payments/payments.routes.ts
+++ server/src/modules/payments/payments.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { createPayment, midtransWebhook } from "./payments.controller.js";
+import { protect } from "../../middlewares/auth.middleware.js";
 
 const paymentsRouter = Router();
 
-paymentsRouter.post("/create", createPayment);
+paymentsRouter.post("/create", protect, createPayment);
 paymentsRouter.post("/webhook", midtransWebhook);
 
 export default paymentsRouter;
